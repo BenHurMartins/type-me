91b7e107204864975f9fe02808f38c185cea10e8
Ben Hur <benhur.martins@hotmail.com>
2023-12-13 16:41:49 -0300

    feat: add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Header />
         {children}
+        <Analytics />
       </body>
     </html>
   );
